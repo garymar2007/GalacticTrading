9344b0d9c936de2eafee1541b2496c01872aedd4
gary.ma <garymar2007@gmail.com>
2024-06-20 16:54:07 +0200

    first commit

---
+++ src/main/java/com/gary/GalacticTrading/GalacticTradingApplication.java
@@ -0,0 +1,13 @@
+package com.gary.GalacticTrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalacticTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalacticTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gary/GalacticTrading/GalacticTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gary.GalacticTrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalacticTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
